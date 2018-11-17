9b8a26c8068de070fa638c318d6726025cdb928a
Aekansh Dixit <aekansh@gmail.com>
2018-11-17 17:00:23 +0530

    Added nav bar and an about comic page.

---
+++ about-comic.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Haruhi Suzumiya Season 3 Script!</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+
+  <body>
+   <div class="container white-bg" style="padding: 15px;">
+
+      <div class="jumbotron" style="padding-top: 10px;">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Author</a>
+            </li>
+          </ul>
+        </nav>
+        <h1 class="display-3">She was serious.</h1>
+        <p class="lead">I'm Haruhi Suzumiya, from East Junior High. First off, I'm not interested in ordinary people. But, if any of you are aliens, time-travelers, or espers, please come see me. That is all!</p>
+        <a class="btn btn-lg btn-success" href="#" role="button">Watch Now!</a>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6 text-justify">
+          <h4>Brief Intro To The Novel</h4>
+          <p>Haruhi Suzumiya (涼宮ハルヒ Suzumiya Haruhi) is a series of light novels written by Nagaru Tanigawa and illustrated by Noizi Ito and which were adapted into other media. Initially released in Japan in 2003 with the novel The Melancholy of Haruhi Suzumiya, it was followed by ten additional volumes, an anime television series adaptation, four manga series, an animated film, two original net animation series and several video games.</p>
+          <p>After the anime adaptation airing in 2006, publishing company Kadokawa Shoten received offers for licensing the novels and their adaptations. The novels are licensed for English language release in the United States by Little, Brown and Company, for young readers by Yen Press and the anime adaptation was licensed for North American distribution by Kadokawa Pictures USA division which then sub-licensed production and distribution to Bandai Entertainment. The anime is currently licensed by Funimation.</p>
+          <h4>Plot</h4>
+          <p>Kyon is a student at North High School in Nishinomiya. He is dragged along by his classmate, the titular character Haruhi Suzumiya, an eccentric schoolgirl who seeks supernatural phenomena and figures such as aliens, time travelers, and espers. With Kyon's reluctant help, Haruhi establishes a club called the "SOS Brigade" (SOS団 Esu-Ō-Esu Dan), short for "Spreading excitement all Over the world with Haruhi Suzumiya Brigade" (世界を大いに盛り上げるための涼宮ハルヒの団 Sekai o Ōini Moriageru Tame no Suzumiya Haruhi no Dan) (In the school's official paperwork Kyon renamed it "Support the Student Body by Overworking to Make the World a Better Place Student Service Brigade") to investigate mysterious events. Haruhi soon recruits three additional members: the laconic bibliophile Yuki Nagato, the shy and timid Mikuru Asahina, and the unflappable transfer student Itsuki Koizumi. These members soon reveal themselves to Kyon, to be the types of extraordinary characters that Haruhi seeks. They have been sent by their respective secret organizations to observe Haruhi — who is unaware that she possesses destructive reality warping powers — and to prevent these powers from being unleashed. Each of the three believe that it would be dangerous were Haruhi to discover she had such powers. Together with Kyon, they work to keep life interesting for Haruhi and to prevent her from becoming bored enough to imagine a new world, as they and their organizations fear that this would destroy the current world.</p>
+        </div>
+
+        <div class="col-lg-6 text-justify">
+          <h4>Reception</h4>
+          <p>The first novel of the series, The Melancholy of Haruhi Suzumiya was awarded the Grand Prize in the eighth annual Sneaker Awards—only the third Grand Prize given out in the Award's history. The series has become a huge success for light novels in Japan, selling over 4,300,000 copies in September 2007 and surpassed 8,000,000 copies when the tenth and eleventh volumes were released in May 2011 after the limited editions set a record 513,000 first pressing for light novels. As of 2017, 20 million copies of all versions of the light novels and manga volumes internationally are in print.</p>
+
+          <h4>Anime</h4>
+          <p>The anime adaptation of The Melancholy of Haruhi Suzumiya (涼宮ハルヒの憂鬱 Suzumiya Haruhi no Yūutsu), produced by the Japanese animation studio Kyoto Animation and directed by Tatsuya Ishihara, contained 14 episodes which aired in Japan between April 2 and July 2, 2006. It was originally aired in a nonlinear order, with the prologue and first seven chapters of the first novel intermixed with chapters from some of the later novels. The "next episode" previews feature two different episode numberings: one number from Haruhi, who numbers the episodes in chronological order, and one number from Kyon, who numbers them in broadcast order. The DVD releases start with "Episode 00" and are then shown in chronological order.</p>
+          <img src="img/disappearance.jpg" class="rounded mx-auto d-block">
+        </div>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; <a href="http://aekansh.in/">Aekansh Dixit</a> 2018</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/login.js"></script>
+  </body>
+</html>
